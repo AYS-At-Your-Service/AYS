1ee59323a4f13e2348e05e669ec7b086276bd14d
Murali Murugan <murali732000@gmail.com>
2024-02-02 17:21:16 +0000

    Rever done in the App.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-
-import './App.css';
+import './App.css'
 
 function App() {
 
-
   return (
-    <div>
-      
-    </div>
-  );
+    <>
+      <h1>AYS</h1>
+    </>
+  )
+
 }
 
-export default App;
+export default App
