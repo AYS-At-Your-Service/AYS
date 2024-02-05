3316518dccb54a83c0c9d40522697a90260caa4b
Venkatesan <aravind.venkatesan@accenture.com>
2024-02-05 22:59:39 +0530

    revert main.jsx changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import SiteHeader from './components/header/header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <ServiceDropdown></ServiceDropdown> */}
-    <SiteHeader></SiteHeader>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
